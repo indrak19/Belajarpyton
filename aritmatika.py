766436d11658bfcafb1116e23509053c1ce534f2
kindra19 <96459383+indrak19@users.noreply.github.com>
2022-05-24 19:43:40 +0700

    Add files via upload

---
+++ aritmatika.py
@@ -0,0 +1,59 @@
+#operasi aritmatika
+#operasi penjumlahan
+a=10
+b=3
+hasil=a+b
+print(a,'+',b,'=',hasil)
+
+#operasi pengurangan
+hasil = a-b
+print(a,'-',b,'=',hasil)
+
+#operasi pengalian
+hail=a*b
+print(a,'*',b,'=',hasil)
+
+#operasi pembagian
+hasil= a/b
+print(a,'/',b,'=',hasil)
+
+#operasi pangkat/exponen
+
+hasil = a**b
+print(a,'^',b,'=',hasil)
+
+#operasi modulus/sisa pembagian
+
+hasil = a % b
+print(a,'%',b,'=',hasil)
+
+#operasi flor division
+
+hasil = a//b
+print(a,'//',b,'=',hasil)
+
+
+#prioritas operasi,operational precedense
+
+x=3
+y=2
+z=4
+
+hasil=x ** y * z + x / y - y % z // x
+print(x,'**',y,'*',z,'+',x, '/', y ,'-',y, '% ',z,'//',x,'hasil',hasil)
+
+
+hasil = x + y * z
+print (x,'+',y,'*',z,'=',hasil)
+
+
+
+
+
+
+
+
+
+
+
+
